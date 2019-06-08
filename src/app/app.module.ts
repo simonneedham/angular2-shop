f3c9d2f9d8bee6fac86a9cad70b1f7444c1d7d5b
Simon Needham <1652096+simonneedham@users.noreply.github.com>
2019-06-08 16:48:18 +0100

    Migrated to HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
@@ -33,8 +33,7 @@ import { CartService } from './cart.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    JsonpModule
+    HttpClientModule
   ],
   providers: [
     DataService,
